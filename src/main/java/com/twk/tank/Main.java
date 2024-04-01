69eb835b2cb88dc83f9ab8f3b5bdbe428dab67b2
Ethan1308982600 <1308982600@qq.com>
2024-04-01 14:39:49 +0800

    加入敌方坦克（5）个

--- src/main/java/com/twk/tank/Main.java
+++ src/main/java/com/twk/tank/Main.java
@@ -3,7 +3,10 @@ package com.twk.tank;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         TankFrame tf = new TankFrame();
-        while(true){
+        for (int i = 0; i < 5; i++) {
+            tf.Tanks.add(new Tank(50 + i * 50, 200, Dir.down, tf));
+        }
+        while (true) {
             Thread.sleep(20);
             tf.repaint();
         }
--- src/main/java/com/twk/tank/TankFrame.java
+++ src/main/java/com/twk/tank/TankFrame.java
@@ -9,9 +9,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TankFrame extends Frame {
-    Tank mytank = new Tank(200, 200, Dir.down,this);
+    Tank mytank = new Tank(200, 400, Dir.down, this);
     List<Bullet> Bullets = new ArrayList<>();
-    static final int GAME_WIDTH = 800,GAMA_HEIGHT = 600;
+    List<Tank> Tanks = new ArrayList<>();
+    static final int GAME_WIDTH = 800, GAMA_HEIGHT = 600;
 
     public TankFrame() {
         this.setSize(GAME_WIDTH, GAMA_HEIGHT);
@@ -26,19 +27,21 @@ public class TankFrame extends Frame {
             }
         });
     }
+
     Image offScreenImage = null;
+
     @Override
-    public void update(Graphics g){
-        if(offScreenImage == null){
-            offScreenImage = this.createImage(GAME_WIDTH,GAMA_HEIGHT);
+    public void update(Graphics g) {
+        if (offScreenImage == null) {
+            offScreenImage = this.createImage(GAME_WIDTH, GAMA_HEIGHT);
         }
         Graphics gOffScreen = offScreenImage.getGraphics();
         Color c = gOffScreen.getColor();
         gOffScreen.setColor(Color.black);
-        gOffScreen.fillRect(0,0,GAME_WIDTH,GAMA_HEIGHT);
+        gOffScreen.fillRect(0, 0, GAME_WIDTH, GAMA_HEIGHT);
         gOffScreen.setColor(c);
         paint(gOffScreen);
-        g.drawImage(offScreenImage,0,0,null);
+        g.drawImage(offScreenImage, 0, 0, null);
     }
 
     @Override
@@ -46,15 +49,19 @@ public class TankFrame extends Frame {
 //        System.out.println("paint");
         Color c = g.getColor();
         g.setColor(Color.white);
-        g.drawString("子弹的数量:" + Bullets.size(),10,60);
+        g.drawString("子弹的数量:" + Bullets.size(), 10, 60);
         mytank.paint(g);
-        for(int i = 0; i < Bullets.size(); i++){
+        for (int i = 0; i < Bullets.size(); i++) {
             Bullets.get(i).paint(g);
+        }
+        for (int i = 0; i < Tanks.size(); i++) {
+            Tanks.get(i).paint(g);
         }
     }
 
     class MyKeyListener extends KeyAdapter {
         boolean bL, bR, bU, bD;
+
         @Override
         public void keyPressed(KeyEvent e) {
             int key = e.getKeyCode();
